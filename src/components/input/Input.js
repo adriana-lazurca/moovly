fa1c1be1ac30bbc3b5fd533f8220ec6e46ec2ef9
Adriana Lazurca <adrianar.lazurca@gmail.com>
2022-02-24 15:31:10 +0100

    validate form

---
+++ src/components/input/Input.js
@@ -0,0 +1,20 @@
+import { ErrorMessage, useField } from 'formik';
+
+import './input.css';
+
+export const Input = ({ label, ...props }) => {
+   const [field, meta] = useField(props);
+
+   return (
+      <div className='mb-2'>
+         <label htmlFor={field.name}>{label}</label>
+         <input
+            className={`form-control ${meta.touched && meta.error && 'is-invalid'}`}
+            {...field}
+            {...props}
+            autoComplete='off'
+         />
+         <ErrorMessage component='div' name={field.name} className='error' />
+      </div>
+   );
+};
